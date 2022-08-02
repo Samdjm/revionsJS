7d4b232f3c5aaee685c0624dcb27637ac546e420
Samdjm <sam.djm93@gmail.com>
2022-08-02 09:49:56 +0200

    Debut J-2

---
+++ public/scripts/1_variables.js
@@ -0,0 +1,7 @@
+//Types chaine de caractères / string:
+// console.log("Une chaine de caractères avec \"double guillement\"");
+console.log('Une chaine de caractères avec "double guillement"');
+console.log("j'ai etudié javascript");
+
+let textVar = "Du texte dans une variables appelée textVar";
+console.log(textVar);
