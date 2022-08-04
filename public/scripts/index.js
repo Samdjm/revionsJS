d2f49d0e1802c672b8420d62967ee0dfe374860c
Samdjm <sam.djm93@gmail.com>
2022-08-04 17:34:33 +0200

    Fin Navbar

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,14 @@
 import { exemple } from "./src/domSelection.js";
 
 exemple();
+
+//Selectionne un l'ement menu button et on ajoute un event listener
+document.querySelector("svg").addEventListener("click", (e) => {
+  console.log(e);
+  const menu = document.querySelector("#menu");
+  if (!menu.style.left || menu.style.left === "-100%") {
+    menu.style.left = "0";
+  } else {
+    menu.style.left = "-100%";
+  }
+});
