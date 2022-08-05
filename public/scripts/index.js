5af6f3ec74a62bfc0623069591dfdeb8e1e39356
Samdjm <sam.djm93@gmail.com>
2022-08-05 10:18:19 +0200

    Fin accordion

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,12 +3,26 @@ import { exemple } from "./src/domSelection.js";
 exemple();
 
 //Selectionne un l'ement menu button et on ajoute un event listener
-document.querySelector("svg").addEventListener("click", (e) => {
-  console.log(e);
+document.querySelector("svg").addEventListener("click", () => {
   const menu = document.querySelector("#menu");
   if (!menu.style.left || menu.style.left === "-100%") {
     menu.style.left = "0";
   } else {
     menu.style.left = "-100%";
   }
+});
+
+document.querySelectorAll(".accordionButton").forEach((htmlElement) => {
+  htmlElement.addEventListener("click", (e) => {
+    console.dir(e.target);
+
+    //const panel = e.target.nextElementSibling;
+    const panel = e.target.parentNode.querySelector(".panel");
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 });
